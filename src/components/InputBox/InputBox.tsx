87e98fa4fddb560b5ac411be67424d8220ac460f
Gabriel Piles <gpiles@thoughtworks.com>
2019-07-25 00:00:06 +0200

    Add inputBox component

---
+++ src/components/InputBox/InputBox.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class InputBox extends Component {
+    render() {
+        return (
+            <div>input</div>
+        )
+    }
+}
+
+export default InputBox;
