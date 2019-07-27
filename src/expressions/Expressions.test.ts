0ebe5cf7e37f0a28a5eb0e429b0993836ec6bb9c
Gabriel Piles <gpiles@thoughtworks.com>
2019-07-27 20:17:41 +0200

    Return value from other cell

--- src/expressions/Expressions.test.ts
+++ src/expressions/Expressions.test.ts
@@ -8,11 +8,11 @@ it('get empty string when no value', () => {
 it('add expressions to expressions', () => {
     const expressions = new Expressions();
 
-    expressions.set('key1', 'value1');
-    expressions.set('key2', 'value2');
+    expressions.set('A1', 'value1');
+    expressions.set('A2', 'value2');
 
-    expect(expressions.get('key1')).toEqual('value1');
-    expect(expressions.get('key2')).toEqual('value2');
+    expect(expressions.get('A1')).toEqual('value1');
+    expect(expressions.get('A2')).toEqual('value2');
 });
 
 describe('Expressions to values', () => {
@@ -20,31 +20,53 @@ describe('Expressions to values', () => {
     it('returns expression when no formula', () => {
         const expressions = new Expressions();
 
-        expressions.set('key1', 'value1');
-        expressions.set('key2', 'value2');
+        expressions.set('A1', 'value1');
+        expressions.set('A2', 'value2');
 
-        expect(expressions.toValues().key1).toEqual( 'value1');
-        expect(expressions.toValues().key2).toEqual( 'value2');
+        expect(expressions.toValues().A1).toEqual( 'value1');
+        expect(expressions.toValues().A2).toEqual( 'value2');
     });
 
     it('returns formula result when it is a number', () => {
         const expressions = new Expressions();
 
-        expressions.set('key1', '=12');
-        expressions.set('key2', '12=');
+        expressions.set('A1', '12=');
+        expressions.set('A2', '=12');
 
-        expect(expressions.toValues().key1).toEqual( '12');
-        expect(expressions.toValues().key2).toEqual( '12=');
+        expect(expressions.toValues().A1).toEqual( '12=');
+        expect(expressions.toValues().A2).toEqual( '12');
+    });
+
+    it('returns formula result without spaces', () => {
+        const expressions = new Expressions();
+
+        expressions.set('A1', '= 1 2');
+        expressions.set('A2', '=21 2  ');
+
+        expect(expressions.toValues().A1).toEqual( '12');
+        expect(expressions.toValues().A2).toEqual( '212');
     });
 
     it('returns #ERROR when the formula is not valid', () => {
         const expressions = new Expressions();
 
-        expressions.set('key1', '=notvalidformula');
-        expressions.set('key2', '=other not valid formul;a');
+        expressions.set('A1', '=notvalidformula');
+        expressions.set('A2', '=other not valid formula');
+        expressions.set('A3', '=1=');
 
-        expect(expressions.toValues().key1).toEqual( '#ERROR');
-        expect(expressions.toValues().key2).toEqual( '#ERROR');
+        expect(expressions.toValues().A1).toEqual( '#ERROR');
+        expect(expressions.toValues().A2).toEqual( '#ERROR');
+        expect(expressions.toValues().A3).toEqual( '#ERROR');
+    });
+
+    it('returns value from other key', () => {
+        const expressions = new Expressions();
+
+        expressions.set('A1', '1');
+        expressions.set('A2', '=A1');
+
+        expect(expressions.toValues().A1).toEqual( '1');
+        expect(expressions.toValues().A2).toEqual( '1');
     });
 
 });
--- src/expressions/Expressions.ts
+++ src/expressions/Expressions.ts
@@ -4,37 +4,46 @@ import {ValuesDictionary} from "./ValueDictionary";
 
 
 class Expressions {
-    private expressions: ExpressionsDictionary = {};
+    private expressionsDictionary: ExpressionsDictionary = {};
 
     public set(key: string, expression: string) {
-        this.expressions[key] = expression;
+        this.expressionsDictionary[key] = expression;
     }
 
     public toValues(): ValuesDictionary {
         const valuesDictionary: ValuesDictionary = {};
 
-        _.each(this.expressions, (expression, key) => valuesDictionary[key] = this.evaluateExpression(expression));
+        _.each(this.expressionsDictionary, (expression, key) => {
+            valuesDictionary[key] = this.evaluateExpression(expression, this.expressionsDictionary)
+        });
 
         return valuesDictionary;
     }
 
-    private evaluateExpression(expression: string): string {
-
+    private evaluateExpression(expression: string, expressionsDictionary: ExpressionsDictionary): string {
         if (/^=/.exec(expression)) {
-            const formula = expression.replace('=', '');
-
-            if (/^[0-9]+$/.exec(formula)){
-                return expression.replace('=', '');
-            }
-
-            return '#ERROR';
+            let formula = expression.replace('=', '');
+            formula = formula.replace(/ /g, '');
+            return this.evaluateFormula(formula, expressionsDictionary);
         }
 
         return expression;
     }
 
+    private evaluateFormula(formula: string, expressionsDictionary: ExpressionsDictionary) : string {
+        if(/^[A-Z]{1}[0-9]{1}/.exec(formula)){
+            return expressionsDictionary[formula];
+        }
+
+        if (/^[0-9]+$/.exec(formula)){
+            return formula;
+        }
+
+        return '#ERROR';
+    }
+
     get(key: string): string {
-        return this.expressions[key] ? this.expressions[key] : '';
+        return this.expressionsDictionary[key] ? this.expressionsDictionary[key] : '';
     }
 }
 
